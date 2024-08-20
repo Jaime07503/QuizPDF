f88efe489b2cb4a36a2d661076ff585b3ab3d4d3
Mario Martinez <mariojaimemartinez27@gmail.com>
2024-08-20 16:47:38 -0600

    Add card icon in jsx file

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CardIcon from "../assets/card";
 import "../Questions.css";
 
 export const Questions = ({ result }) => {
@@ -57,9 +58,7 @@ export const Questions = ({ result }) => {
 
   return (
     <section className="quiz__container">
-      <div>
-        <p className="summary__content">{result.summary.content}</p>
-      </div>
+      <p className="summary__content">{result.summary.content}</p>
 
       <div className="grid__container">
         <div className="memory__grid">
@@ -72,28 +71,7 @@ export const Questions = ({ result }) => {
               onClick={() => handleCardClick(index)}
             >
               <div className="card__content">
-                {isFlipped(index) ? (
-                  card.text
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="icon icon-tabler icon-tabler-play-card"
-                    width="44"
-                    height="44"
-                    viewBox="0 0 24 24"
-                    stroke-width="0.5"
-                    stroke="#d75a5a"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M19 5v14a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2z" />
-                    <path d="M8 6h.01" />
-                    <path d="M16 18h.01" />
-                    <path d="M12 16l-3 -4l3 -4l3 4z" />
-                  </svg>
-                )}
+                {isFlipped(index) ? card.text : <CardIcon />}
               </div>
             </div>
           ))}
